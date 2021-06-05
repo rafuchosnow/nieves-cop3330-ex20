ee5ff5668cfd56147e66f622ba8832288d361826
rafuchosnow <rafuchosnow@gmail.com>
2021-06-05 17:17:29 -0400

    Initial Commit

---
+++ src/main/java/org/example/MultistateSalesTax.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class MultistateSalesTax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DecimalFormat dfCent = new DecimalFormat("#.##");
+
+        System.out.print("What is the order amount? ");
+        String orderString = input.nextLine();
+        double orderNumber = Double.parseDouble(orderString);
+
+        System.out.print("What state do you live in? ");
+        String stateString = input.nextLine();
+
+        double tax = 0 , total;
+
+        if (stateString.equals("Wisconsin")) {
+            System.out.print("What county do you live in? ");
+            String countyString = input.nextLine();
+            tax = 0.05  * orderNumber;
+
+            if (countyString.equals("Eau Claire")) {
+                tax = (0.05 + 0.005) * orderNumber;
+            }
+            if (countyString.equals("Dunn")) {
+                tax = (0.05 + 0.004) * orderNumber;
+            }
+        }
+        if (stateString.equals("Illinois")) {
+            tax = 0.08 * orderNumber;
+        }
+
+        total = orderNumber + tax;
+
+        System.out.println("The tax is $" + dfCent.format(tax) + ".\n" +
+                           "The total is $" + dfCent.format(total) + ".");
+    }
+}
